5bbc8994c4e7de34db625ccfbfbf6dce92c045c2
Josh Graham <joshua.h.graham96@gmail.com>
2021-10-09 18:01:29 -0400

    Adds ngFor to reduce redundant code

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,13 +1,14 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { MatSliderModule } from '@angular/material/slider';
+import { Component } from '@angular/core';
 import { TurnService } from '../turn.service';
+
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  providers: [TurnService]
+  providers: [TurnService],
 })
 export class SidebarComponent {
+  options: Array<string> = ["L", "R", "U", "D", "F", "B", "l", "r", "u", "d", "f", "b", "M", "x", "y", "z"];
   constructor(private turnService: TurnService)
   {
     this.turnService = turnService;
