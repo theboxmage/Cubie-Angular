c639f669dad2d05e3dfb015d5a8d985b6c1b2c96
Josh Graham <joshua.h.graham96@gmail.com>
2021-10-09 17:59:47 -0400

    Fixes formatting to match future linter

--- src/app/canvas/Cube.ts
+++ src/app/canvas/Cube.ts
@@ -1,133 +1,133 @@
 import * as p5 from "p5";
-import { TurnService } from "../turn.service";
-import { AnimationService } from "./animationService";
-import { Cubie } from './Cubie';
+import {TurnService} from "../turn.service";
+import {AnimationService} from "./animationService";
+import {Cubie} from './Cubie';
+
 export class Cube {
-    animationService: AnimationService;
-    queue: string[];
-    angle = (Math.PI / 2);
-    cubes: Cubie[];
-    constructor(private canvas: p5, private width: number, private turnService: TurnService) {
-        this.cubes = new Array(27);
-        this.animationService = new AnimationService(this.cubes, this.turnService);
-        this.queue = [];
-        let count: number = 0;
-        for (let x = -1; x <= 1; x++) {
-            for (let y = -1; y <= 1; y++) {
-                for (let z = -1; z <= 1; z++) {
-                    this.cubes[count] = new Cubie(x, y, z, 50, canvas);
-                    count++;
-                }
-            }
-        }
-        turnService.animationFinished$.subscribe( () => {
-            this.queue.shift();
-            this.stepAlg();
-        });
-    }
+  animationService: AnimationService;
+  queue: string[];
+  angle = (Math.PI / 2);
+  cubes: Cubie[];
 
-    show() {
-        for (let x = 0; x < 27; x++) {
-            this.cubes[x].show();
+  constructor(private canvas: p5, private width: number, private turnService: TurnService) {
+    this.cubes = new Array(27);
+    this.animationService = new AnimationService(this.cubes, this.turnService);
+    this.queue = [];
+    let count: number = 0;
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        for (let z = -1; z <= 1; z++) {
+          this.cubes[count] = new Cubie(x, y, z, 50, canvas);
+          count++;
         }
+      }
     }
+    turnService.animationFinished$.subscribe(() => {
+      this.queue.shift();
+      this.stepAlg();
+    });
+  }
 
-    loadAlg(turn: string) {
-        this.queue.push(turn);
-        if(this.queue.length == 1)
-        {
-            this.stepAlg();
-        }
+  show() {
+    for (let x = 0; x < 27; x++) {
+      this.cubes[x].show();
     }
+  }
 
-    stepAlg() {
-        let nextStep = this.queue[0];
-        if(nextStep)
-        {
-            this.applyTurn(nextStep);
-        }
+  loadAlg(turn: string) {
+    this.queue.push(turn);
+    if (this.queue.length == 1) {
+      this.stepAlg();
     }
+  }
 
-    applyTurn(turn: string) {
-        switch (turn) {
-            case 'R':
-                this.animationService.turnX([1], 1);
-                break;
-            case 'R\'':
-                this.animationService.turnX([1], -1);
-                break;
-            case 'r':
-                this.animationService.turnX([0, 1], 1);
-                break;
-            case 'r\'':
-                this.animationService.turnX([0, 1], -1);
-                break;
-            case 'L':
-                this.animationService.turnX([-1], -1);
-                break;
-            case 'L\'':
-                this.animationService.turnX([-1], 1);
-                break;
-            case 'l':
-                this.animationService.turnX([0, -1], -1);
-                break;
-            case 'l\'':
-                this.animationService.turnX([0, -1], 1);
-                break;
-            case 'U':
-                this.animationService.turnY([-1], -1);
-                break;
-            case 'U\'':
-                this.animationService.turnY([-1], 1);
-                break;
-            case 'u':
-                this.animationService.turnY([0, -1], -1);
-                break;
-            case 'u\'':
-                this.animationService.turnY([0, -1], 1);
-                break;
-            case 'D':
-                this.animationService.turnY([1], 1);
-                break;
-            case 'D\'':
-                this.animationService.turnY([1], -1);
-                break;
-            case 'd':
-                this.animationService.turnY([0, 1], 1);
-                break;
-            case 'd\'':
-                this.animationService.turnY([0, 1], -1);
-                break;
-            case 'F':
-                this.animationService.turnZ([1], 1);
-                break;
-            case 'F\'':
-                this.animationService.turnZ([1], -1);
-                break;
-            case 'f':
-                this.animationService.turnZ([0, 1], 1);
-                break;
-            case 'f\'':
-                this.animationService.turnZ([0, 1], -1);
-                break;
-            case 'B':
-                this.animationService.turnZ([-1], -1);
-                break;
-            case 'B\'':
-                this.animationService.turnZ([-1], 1);
-                break;
-            case 'b':
-                this.animationService.turnZ([0, -1], -1);
-                break;
-            case 'b\'':
-                this.animationService.turnZ([0, -1], 1);
-                break;
-            case 'M':
-                this.animationService.turnX([0], -1);
-                break;
-            case 'M\'':
-                this.animationService.turnX([0], 1);
-                break;
-        }
+  stepAlg() {
+    let nextStep = this.queue[0];
+    if (nextStep) {
+      this.applyTurn(nextStep);
     }
+  }
+
+  applyTurn(turn: string) {
+    switch (turn) {
+      case 'R':
+        this.animationService.turnX([1], 1);
+        break;
+      case 'R\'':
+        this.animationService.turnX([1], -1);
+        break;
+      case 'r':
+        this.animationService.turnX([0, 1], 1);
+        break;
+      case 'r\'':
+        this.animationService.turnX([0, 1], -1);
+        break;
+      case 'L':
+        this.animationService.turnX([-1], -1);
+        break;
+      case 'L\'':
+        this.animationService.turnX([-1], 1);
+        break;
+      case 'l':
+        this.animationService.turnX([0, -1], -1);
+        break;
+      case 'l\'':
+        this.animationService.turnX([0, -1], 1);
+        break;
+      case 'U':
+        this.animationService.turnY([-1], -1);
+        break;
+      case 'U\'':
+        this.animationService.turnY([-1], 1);
+        break;
+      case 'u':
+        this.animationService.turnY([0, -1], -1);
+        break;
+      case 'u\'':
+        this.animationService.turnY([0, -1], 1);
+        break;
+      case 'D':
+        this.animationService.turnY([1], 1);
+        break;
+      case 'D\'':
+        this.animationService.turnY([1], -1);
+        break;
+      case 'd':
+        this.animationService.turnY([0, 1], 1);
+        break;
+      case 'd\'':
+        this.animationService.turnY([0, 1], -1);
+        break;
+      case 'F':
+        this.animationService.turnZ([1], 1);
+        break;
+      case 'F\'':
+        this.animationService.turnZ([1], -1);
+        break;
+      case 'f':
+        this.animationService.turnZ([0, 1], 1);
+        break;
+      case 'f\'':
+        this.animationService.turnZ([0, 1], -1);
+        break;
+      case 'B':
+        this.animationService.turnZ([-1], -1);
+        break;
+      case 'B\'':
+        this.animationService.turnZ([-1], 1);
+        break;
+      case 'b':
+        this.animationService.turnZ([0, -1], -1);
+        break;
+      case 'b\'':
+        this.animationService.turnZ([0, -1], 1);
+        break;
+      case 'M':
+        this.animationService.turnX([0], -1);
+        break;
+      case 'M\'':
+        this.animationService.turnX([0], 1);
+        break;
+    }
+  }
 }
