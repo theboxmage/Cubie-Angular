3f05c662a1628aeb8b56e69db82da62dcef2e069
Josh Graham <joshua.h.graham96@gmail.com>
2021-10-11 11:48:54 -0400

    Adds ability to turn the Cube

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -13,6 +13,7 @@ import {FormControl} from "@angular/forms";
 export class CanvasComponent implements OnInit {
   cube!: Cube;
   @Input() control!: FormControl;
+
   constructor(private turnService: TurnService) {
     turnService.turnSource$.subscribe(
       turn => {
@@ -22,21 +23,48 @@ export class CanvasComponent implements OnInit {
 
 
   ngOnInit(): void {
+    let canvasX = Math.PI / -10;
+    let canvasY = Math.PI / -6;
     const sketch = (canvas_: p5) => {
       canvas_.setup = () => {
         let canvas = canvas_.createCanvas(400, 400, canvas_.WEBGL);
         this.cube = new Cube(canvas_, 50, this.turnService, this.control);
         canvas.parent('sketch-holder');
+
+        let drag = false;
+        let x = 0, y = 0;
+        let dx = y, dy = 0;
+        canvas.mousePressed(() => {
+          drag = true;
+          x = canvas_.mouseX;
+          y = canvas_.mouseY;
+        });
+
+        canvas.mouseReleased(() => {
+          drag = false;
+        });
+
+        canvas.mouseMoved(() => {
+          if (drag) {
+            dy = (canvas_.mouseX-x)/100;
+            dx = (canvas_.mouseY-y)/-100;
+            x = canvas_.mouseX;
+            y = canvas_.mouseY;
+            canvasX += dx;
+            canvasY += dy
+          }
+        });
       };
 
       canvas_.draw = () => {
-        canvas.rotateX(canvas_.PI / -10)
-        canvas.rotateY(canvas_.PI / -6)
+        canvas.rotateX(canvasX)
+        canvas.rotateY(canvasY)
         canvas_.clear();
         this.cube.show();
       };
     }
     let canvas = new p5(sketch);
+
   }
 
 }
